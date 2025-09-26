07287e73a14385626b558d181cd7675e687e908c
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-09-26 22:44:55 +0100

    Read Numbers from N to 1

---
+++ Cpp-Area/01-Problem-Solving/Problem#27.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumber()
+{
+	int Num;
+
+	cout << "Enter a Number: ";
+	cin >> Num;
+	return Num;
+}
+
+void PrintRangeFromNto1_UsingWhile(int N)
+{
+	int Counter = N + 1;
+
+	cout << "Range printed using While Statement:\n";
+	while (Counter > 1)
+	{
+		Counter--;
+		cout << Counter << endl;
+	}
+}
+
+void PrintRangeFromNto1_UsingDoWhile(int N)
+{
+	int Counter = N + 1;
+
+	cout << "Range printed using Do..While Statement:\n";
+	do
+	{
+		Counter--;
+		cout << Counter << endl;
+	} while (Counter > 1);
+}
+
+void PrintRangeFromNto1_UsingFor(int N)
+{
+	cout << "Range printed using For Statement:\n";
+
+	for (int Counter = N; Counter >= 1; Counter--)
+	{
+		cout << Counter << endl;
+	}
+}
+
+int main()
+{
+	int N = ReadNumber();
+
+	PrintRangeFromNto1_UsingWhile(N);
+	PrintRangeFromNto1_UsingDoWhile(N);
+	PrintRangeFromNto1_UsingFor(N);
+
+	return 0;
+}
