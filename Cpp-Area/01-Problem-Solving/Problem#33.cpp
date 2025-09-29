174eded94ac8cd6fbd854bb2a93d33739765046d
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-09-29 22:00:13 +0100

    Grade A, B, C, D, E, F

---
+++ Cpp-Area/01-Problem-Solving/Problem#33.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumberInRange(int From, int To)
+{
+	int	Grade;
+	do
+	{
+		cout << "Enter a Grade between 0 and 100: ";
+		cin >> Grade;
+	} while (Grade < From || Grade > To);
+
+	return Grade;
+}
+
+char GetGradeLetter(int Grade)
+{
+	if (Grade >= 90)
+		return 'A';
+	else if (Grade >= 80)
+		return 'B';
+	else if (Grade >= 70)
+		return 'C';
+	else if (Grade >= 60)
+		return 'D';
+	else if (Grade >= 50)
+		return 'E';
+	else
+		return 'F';
+}
+
+int main()
+{
+	cout << endl << "Result = " << GetGradeLetter(ReadNumberInRange(0, 100));
+
+	return 0;
+}
