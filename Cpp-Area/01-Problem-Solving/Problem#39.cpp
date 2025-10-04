ec20e58c14f834398c73a9507b36b2235357e321
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-10-04 01:22:16 +0100

    Pay Remainder

---
+++ Cpp-Area/01-Problem-Solving/Problem#39.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+    float Number = 0;
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+float CalculateRemainder(float TotalBill, float TotalCashPaid)
+{
+    return TotalCashPaid - TotalBill;
+}
+
+int main()
+{
+    float TotalBill = ReadPositiveNumber("Enter Total Bill: ");
+    float TotalCashPaid = ReadPositiveNumber("Enter Total Cash Paid: ");
+    cout << endl;
+
+    cout << "Total Bill = " << TotalBill << endl;
+    cout << "Total Cash Paid = " << TotalCashPaid << endl;
+    cout << "***************************\n";
+    cout << "Remainder = " << CalculateRemainder(TotalBill, TotalCashPaid) << endl;
+
+    return 0;
+}
