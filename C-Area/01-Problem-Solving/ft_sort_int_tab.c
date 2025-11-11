b7c7d75e36b27ab561f9e225e04211ca281e9ef1
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-11-11 21:30:59 +0100

    sort int tab

---
+++ C-Area/01-Problem-Solving/ft_sort_int_tab.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soel-had <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 11:26:28 by soel-had          #+#    #+#             */
+/*   Updated: 2025/08/18 11:27:21 by soel-had         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size -1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
