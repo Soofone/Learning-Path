bfa4cb9af6fd85f21dc8e6f71828585ff0be868d
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-11-30 20:59:33 +0100

    print string (non-printable characters in hex)

---
+++ C-Area/01-Problem-Solving/ft_putstr_non_printable.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soel-had <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 16:36:23 by soel-had          #+#    #+#             */
+/*   Updated: 2025/08/21 08:24:12 by soel-had         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int		i;
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	i = 0;
+	while (str[i])
+	{
+		if (!(32 <= (unsigned char)str[i] && (unsigned char)str[i] <= 126))
+		{
+			ft_putchar('\\');
+			ft_putchar(hex[(unsigned char)str[i] / 16]);
+			ft_putchar(hex[(unsigned char)str[i] % 16]);
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+}
