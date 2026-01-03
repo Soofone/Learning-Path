e810232608c9a9c57f08f35b62d434d50a8c8905
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2026-01-03 22:42:52 +0100

    3x3 Random Matrix

---
+++ Cpp-Area/03-Problem-Solving/Problem#01.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	return rand() % (To - From + 1);
+}
+
+void FillMatrixWhithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			arr[i][j] = RandomNumber(1, 100);
+		}
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			cout << setw(3) << arr[i][j] << "     ";
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[3][3];
+	
+	FillMatrixWhithRandomNumbers(arr, 3, 3);
+	cout << "\n The following is a 3x3 random matrix:\n";
+	PrintMatrix(arr, 3, 3);
+
+	return 0;
+}
