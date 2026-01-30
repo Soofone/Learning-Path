ac78b3692b0250b620e9e10c21b2436f2fe480d8
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2026-01-30 22:57:12 +0100

    Invert Character Case

---
+++ Cpp-Area/03-Problem-Solving/Problem#27.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char ReadChar()
+{
+	char Ch1 = ' ';
+	cout << "Please Enter a Character?\n";
+	cin >> Ch1;
+
+	return Ch1;
+}
+
+char InvertLetterCase(char Char1)
+{
+	return isupper(Char1) ? tolower(Char1) : toupper(Char1);
+}
+
+int main()
+{
+	char Ch1 = ReadChar();
+	cout << "\nChar After Inverting Case:\n";
+
+	Ch1 = InvertLetterCase(Ch1);
+	cout << Ch1 << endl;
+
+	system("pause>0");
+}
