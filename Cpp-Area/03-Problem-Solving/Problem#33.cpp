efa5571c9993aeea45661c77db2001c3aef7efff
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2026-02-05 22:06:55 +0100

    Count Vowel

---
+++ Cpp-Area/03-Problem-Solving/Problem#33.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadString()
+{
+	string S1 = "";
+	cout << "Please Enter Your String?\n";
+	getline(cin, S1);
+
+	return S1;
+}
+
+bool IsVowel(char Ch1)
+{
+	Ch1 = tolower(Ch1);
+
+	return ((Ch1 == 'a') || (Ch1 == 'e') || (Ch1 == 'i') || (Ch1 == 'o') || (Ch1 == 'u'));
+}
+
+short CountVowels(string S1)
+{
+	short Counter = 0;
+
+	for (short i = 0; i < S1.length(); i++)
+	{
+		if (IsVowel(S1[i]))
+		{
+			Counter++;
+		}
+	}
+	return Counter;
+}
+
+int main()
+{
+	string S1 = ReadString();
+
+	cout << "\nNumber of Vowels is: " << CountVowels(S1);
+
+	system("pause>0");
+}
