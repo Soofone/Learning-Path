9f0197b9dac31f38e9b99244d2d852c1ad1e3156
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2026-03-13 21:24:18 +0000

    Number of Days In a Month Short Logic

---
+++ Cpp-Area/04-Problem-Solving/Problem#06.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool IsLeapYear(short Year)
+{
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+	if (Month < 1 || Month > 12)
+		return 0;
+
+	int NumberOfDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+	return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+short ReadYear()
+{
+	short Year = 0;
+	cout << "Please enter a year to check? ";
+	cin >> Year;
+	return Year;
+}
+
+short ReadMonth()
+{
+	short Month = 0;
+	cout << "\nPlease enter a month to check? ";
+	cin >> Month;
+	return Month;
+}
+
+int main()
+{
+	short Year = ReadYear();
+	short Month = ReadMonth();
+
+	cout << "\nNumber of Days in Month [" << Month << "] is " << NumberOfDaysInAMonth(Month, Year);
+
+
+	system("pause>0");
+	return 0;
+}
