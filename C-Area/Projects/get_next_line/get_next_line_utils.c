117be5b107976ace1baa7f347a4c30360b0d20be
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2026-05-17 09:12:48 +0100

    Add utility functions for get_next_line

---
+++ C-Area/Projects/get_next_line/get_next_line_utils.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soel-had <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:52:48 by soel-had          #+#    #+#             */
+/*   Updated: 2025/11/27 18:01:34 by soel-had         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	new_line_found(char *line)
+{
+	size_t	i;
+
+	if (!line)
+		return (0);
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc(s1_len + s2_len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < s2_len)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+void	*ft_memcpy(void *dest, void *src, size_t n)
+{
+	unsigned char		*dest2;
+	unsigned char		*src2;
+	size_t				i;
+
+	if (dest == src)
+		return (dest);
+	dest2 = (unsigned char *)dest;
+	src2 = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		dest2[i] = src2[i];
+		i++;
+	}
+	return (dest);
+}
